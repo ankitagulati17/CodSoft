7d78b02f0d4c954086be47069b2f5ca9a8db1621
ankitagulati17 <141409175+ankitagulati17@users.noreply.github.com>
2023-09-29 02:41:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,118 @@
-// nav background
-let header = document.querySelector("header");
 
-window.addEventListener("scroll", () => {
-    header.classList.toggle("shadow", window.scrollY > 0)
-})
+let card = document.querySelector(".trend");
+let card2 = document.getElementById("trendSec");
+let about = document.querySelector(".about");
+let contact = document.querySelector(".contact");
 
-//Filter
-$(document).ready(function () {
-    $(".filter-item").click(function () {
-        const value = $(this).attr("data-filter");
-        if (value == "all"){
-            $(".post-box").show("1000")
-        } else{
-            $(".post-box")
-                .not("." + value)
-                .hide(1000);
-            $(".post-box")
-            .filter("." + value)
-            .show("1000")
-        }
-    });
-    $(".filter-item").click(function () {
-        $(this).addClass("active-filter").siblings().removeClass("active-filter")
-    });
-});
+
+console.log(card);
+let blog = document.querySelector(".trends");
+let mainPage = document.querySelector(".main");
+
+function homes(){
+    mainPage.style.display="flex";
+    card.style.display="block";
+    card2.style.display="block";
+    blog.style.display="block";
+   about.style.display="none"
+
+
+    
+document.getElementById("blog").style.color="black";
+document.getElementById("home").style.color="rgb(2, 173, 173)";
+document.getElementById("shop").style.color="black";
+document.getElementById("contact").style.color="black";
+document.getElementById("about").style.color="black"
+
+
+}
+function shops(){
+mainPage.style.display="none";
+blog.style.display="none";
+about.style.display="none"
+card.style.display="block";
+card2.style.display="block";
+
+ 
+document.getElementById("blog").style.color="black";
+document.getElementById("home").style.color="black";
+document.getElementById("shop").style.color="rgb(2, 173, 173)";
+document.getElementById("contact").style.color="black";
+document.getElementById("about").style.color="black"
+
+
+}
+
+function blogs(){
+    mainPage.style.display="none";
+    card.style.display="none";
+    card2.style.display="none";
+   blog.style.display="block";
+   about.style.display="none"
+    
+
+document.getElementById("blog").style.color="rgb(2, 173, 173)";
+document.getElementById("home").style.color="black";
+document.getElementById("shop").style.color="black";
+document.getElementById("contact").style.color="black";
+document.getElementById("about").style.color="black"
+
+ 
+
+
+
+}
+
+function abouts(){
+    mainPage.style.display="none";
+    card.style.display="none";
+    card2.style.display="none";
+   blog.style.display="none";
+   about.style.display="block"
+
+document.getElementById("blog").style.color="black";
+document.getElementById("home").style.color="black";
+document.getElementById("shop").style.color="black";
+document.getElementById("contact").style.color="black";
+document.getElementById("about").style.color="rgb(2, 173, 173)"
+
+}
+
+function contacts(){
+    mainPage.style.display="none";
+    card.style.display="none";
+    card2.style.display="none";
+   blog.style.display="none";
+   about.style.display="none";
+   contact.style.display="block"
+
+document.getElementById("blog").style.color="black";
+document.getElementById("home").style.color="black";
+document.getElementById("shop").style.color="black";
+document.getElementById("contact").style.color="black";
+document.getElementById("about").style.color="black"
+document.getElementById("contact").style.color="rgb(2, 173, 173)"
+
+}
+
+// cart
+
+function show(img){
+    let newImg = document.getElementById("newImg");
+    console.log(img);
+    newImg.src=img.src;
+
+    mainPage.style.display="none";
+    card.style.display="none";
+    card2.style.display="none";
+   blog.style.display="none";
+   about.style.display="none";
+   contact.style.display="none"
+    document.querySelector(".cart").style.display="flex"
+}
+
+
+function addCart(){
+    alert("Added To Cart");
+    location.reload()
+}
